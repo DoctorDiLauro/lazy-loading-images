84a9b22cf8d5fbb0184e8a90e496d1de5583e7f3
DoctorDiLauro <dilauro.chris@gmail.com>
2017-12-20 15:35:29 -0500

    js fix

--- js/image-loader.js
+++ js/image-loader.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function (e) {
   var imgs = document.querySelectorAll('.non-critical-img');
 
   [].forEach.call(imgs, function (img) {
-    var imgTag = img.quesrySelector('img');
+    var imgTag = img.querySelector('img');
 
     imgTag.src = imgTag.dataset.src;
     imgTag.removeAttribute('hidden') ;
